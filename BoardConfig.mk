86450fa24e7e68e1575751c33a4e1fbec1c863e3
Harald Weber <harald.h.weber@gmail.com>
2014-12-18 17:11:46 +0000

    added device name aries
    Change-Id: Ie8f04bc8a89e64c88e0798ba7415957cb2d25c68

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ include device/sony/shinano-common/BoardConfigCommon.mk
 #-include vendor/sony/z3c/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := D5803,D5833,z3c
+TARGET_OTA_ASSERT_DEVICE := D5803,D5833,z3c,aries
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/z3c/include
 
