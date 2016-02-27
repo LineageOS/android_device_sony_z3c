5c3302ee0c0dd072dfa2d66f370fbd8ba8e496dc
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-02-27 12:21:01 -0300

    z3c: move touchscreen *.idc to device tree
    Change-Id: Ic64cb7bea6c8299fd6df1a26fe27bef6aa175fd0

--- z3c.mk
+++ z3c.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/clearpad.idc:system/usr/idc/clearpad.idc
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
