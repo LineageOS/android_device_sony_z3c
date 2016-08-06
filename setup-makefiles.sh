795b6bce2fc9c73a0c9cb4ec6c5a522770122191
Steve Kondik <steve@cyngn.com>
2016-08-06 02:40:08 -0700

    z3c: Use generic extract utils
    Change-Id: Ia5e025fe984a854331d3d47daefe95cf2dea7499

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,6 +21,8 @@ FP=$(cd ${0%/*} && pwd -P)
 export DEVICE=$(basename $FP)
 export VENDOR=$(basename $(dirname $FP))
 
+export INITIAL_COPYRIGHT_YEAR=2014
+
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
@@ -34,25 +36,17 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
+# Copyright headers and guards
+write_headers
 
-"$MY_DIR"/setup-makefiles.sh
+# The standard blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Vendor BoardConfig variables
+printf 'USE_CAMERA_STUB := false\n' >> "$BOARDMK"
+
+# We are done!
+write_footers
