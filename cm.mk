bb611d573ae46e2ee43c50995f8b3dd714ac4aed
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 02:36:33 -0700

    z3c: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8
    (cherry picked from commit 0d84f601978a9815e628c6d8ccc0f75b27cd29f6)

--- cm.mk
+++ cm.mk
@@ -1,9 +1,6 @@
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/sony/z3c/full_z3c.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
