7880df200e5ee22a6d2cbba94b61170c1158f2ee
nailyk-fr <nailyk_git@nailyk.fr>
2017-03-26 20:04:32 +0200

    z3c: Add D2TW node
    Change-Id: Ic9788c8be9cac1ce32cff90658e39c62c99e1405

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,9 @@ TARGET_USE_SDCLANG := true
 BOARD_VOLD_MAX_PARTITIONS := 25
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12656242688 # 12656259072 - 16384
 
+#D2TW
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/clearpad/wakeup_gesture"
+
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     device/sony/z3c/sepolicy
