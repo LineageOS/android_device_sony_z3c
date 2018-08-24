c6eda2221e4a512a1221fa59d0bc50a50646633e
Danny Baumann <dannybaumann@web.de>
2018-08-24 09:01:55 +0200

    z3c: Consider lineage_z3c product via the AOSP way
    Change-Id: I085b2a6f93b5e70c38281038b8a83b467824eda1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
-# Copyright (C) 2013 The CyanogenMod Project
+#
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from z3c device
-$(call inherit-product, device/sony/z3c/z3c.mk)
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_z3c
-PRODUCT_DEVICE := z3c
-PRODUCT_BRAND := Sony
-PRODUCT_MANUFACTURER := Sony
-PRODUCT_MODEL := Xperia Z3C
+PRODUCT_MAKEFILES := \
+	 $(LOCAL_DIR)/lineage_z3c.mk
---
+++ lineage_z3c.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from z3c device
+$(call inherit-product, device/sony/z3c/z3c.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_z3c
+PRODUCT_DEVICE := z3c
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia Z3 Compact
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="D5803-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
+
+BUILD_FINGERPRINT := Sony/D5803/D5833:6.0.1/23.5.A.1.291/2769308465:user/release-keys
